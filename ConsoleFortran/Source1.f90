5dcbd291818f5edd9661dcbf50d515aaf27e1f2b
jg-tubt <42897138+jg-tubt@users.noreply.github.com>
2018-09-02 17:06:38 +0200

    Update Source1.f90

--- ConsoleFortran/Source1.f90
+++ ConsoleFortran/Source1.f90
@@ -7,4 +7,6 @@ program prueba
     
     write(*,fmt="(A,F0.3)") 'Area de un rectangulo de base 3 y altura 4: ', reactangleArea(3.0, 4.0)
     
+    write(*,fmt="(A)") 'All operations have ended'
+    
 end program
