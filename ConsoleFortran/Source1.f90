19b1122597da5864844a8ede74b85271e70bb665
jg-tubt <42897138+jg-tubt@users.noreply.github.com>
2018-09-02 17:04:05 +0200

    Update Source1.f90

--- ConsoleFortran/Source1.f90
+++ ConsoleFortran/Source1.f90
@@ -3,7 +3,7 @@ program prueba
     
     write(*,fmt="(A)") 'starting operations'
     
-    write(*,fmt="(A,F0.3)") 'Area de un circulo de radio 3: ', circleArea(3.0)
+    write(*,fmt="(A,F0.3)") 'Area de un circulo de radio 3:              ', circleArea(3.0)
     
     write(*,fmt="(A,F0.3)") 'Area de un rectangulo de base 3 y altura 4: ', reactangleArea(3.0, 4.0)
     
